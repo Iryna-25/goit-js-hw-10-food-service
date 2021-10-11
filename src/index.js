e6a9ab005b480a6463ab469a69a26f3394ebf78c
Iryna Shandar <iryna.shandar@gmail.com>
2021-10-11 23:42:02 +0300

    js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './sass/main.scss';
+import template from './menu-template.hbs';
+import menu from './menu.json';
+
+const menuRef = document.querySelector('.js-menu');
+
+const markup = template(menu);
+menuRef.insertAdjacentHTML('beforeend', markup);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const bodyRef = document.querySelector('body');
+const themeSwitch = document.querySelector('.theme-switch__toggle');
+
+bodyRef.classList.add(localStorage.getItem('theme') || Theme.LIGHT);
+
+if (bodyRef.classList.contains(Theme.DARK)) {
+  themeSwitch.checked = true;
+}
+
+themeSwitch.addEventListener('change', switchTheme);
+
+function switchTheme() {
+  if (bodyRef.classList.contains(Theme.LIGHT)) {
+    bodyRef.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('theme', 'dark-theme');
+  } else if (bodyRef.classList.contains(Theme.DARK)) {
+    bodyRef.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('theme', 'light-theme');
+  }
+}
